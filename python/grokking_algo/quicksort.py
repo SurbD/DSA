f754bfe75db96dd0a04af92f5d06b5d4dc092faf
Divine Mukoro <surbvinex@gmail.com>
2024-01-06 06:33:55 +0100

    Added quicksort function from grokking algo

---
+++ python/grokking_algo/quicksort.py
@@ -0,0 +1,15 @@
+"""
+Implementation of quicksort algorithm from Grokking Algorithm book
+"""
+
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        greater = [i for i in array[1:] if i > pivot]
+
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+print(quicksort([23, 12, 51, 5]))
